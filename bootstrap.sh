746d68330d83cd8d6791a96ad2b7fd6c2b300aed
BobleChinois <674694@protonmail.ch>
2019-09-25 21:30:32 +0200

    working

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3 libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler git ccache
+sudo apt-get update && sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config bsdmainutils python3 libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler git ccache
 
-cd /bitcoin
+# cd /bitcoin
 
-./contrib/install_db4.sh `pwd`
+# ./contrib/install_db4.sh `pwd`
 
-export BDB_PREFIX='/bitcoin/db4'
+# export BDB_PREFIX='/bitcoin/db4'
